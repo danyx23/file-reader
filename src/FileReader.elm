e13b88582e31de5c6e07d56f8b5031330f8315e8
Simon Hampton <hotbelgo@gmail.com>
2015-10-08 20:05:12 +0200

    1.0.0

--- src/FileReader.elm
+++ src/FileReader.elm
@@ -1,8 +1,8 @@
-module FileReader where
+module FileReader (getTextFile) where
 {-| Elm bindings to HTML5 Reader API.
 
-# Read text (not binary) file as text string
-@docs getFileContents
+# Read file as text string
+@docs getTextFile
 
 -}
 
@@ -11,10 +11,10 @@ import Task exposing (Task)
 
 import Native.FileReader
 
-{-| Takes the id of an `input` of `type="file"`
-and attempts to read the file associated with it by the user.
+{-| Takes the id of an `input` of `type="file"` and attempts
+to read the text file associated with it by the user.
 
-    getFileContents "upload"
+    getTextFile "upload"
 -}
 getTextFile : String -> Task error String
 getTextFile = Native.FileReader.getTextFile
